75925d451a0092c97d21add72ff6225a4106b85c
Brijmohan Dammani <Brijmohan.Dammani@programmers.io>
2024-09-30 19:12:44 +0530

    added azure connectionstring

--- src/CleanArchitecture.Web/Temp.cs
+++ src/CleanArchitecture.Web/Temp.cs
@@ -8,10 +8,12 @@ namespace CleanArchitecture.Web
             // Hardcoded secret information with common patterns
             string apiKey = "AKIAIOSFODNN7EXAMPLE"; // AWS Access Key ID
             string password = "P@ssw0rd1234!"; // Example password
+            string azureSASConnectionString = "sv=2015-04-05&sr=b&si=tutorial-policy-635959936145100803&sig=9aCzs76n0E7y5BpEi2GvsSv433BZa22leDOZXX%2BXXIU%3D";
 
             Console.WriteLine("Hello, World!");
             Console.WriteLine($"API Key: {apiKey}");
             Console.WriteLine($"Password: {password}");
+            Console.WriteLine($"azureSASConnectionString: {azureSASConnectionString}");
         }
     }
 }
