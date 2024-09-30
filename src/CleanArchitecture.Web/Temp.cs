9a4b3af71f023f6c7670f0e1501b1721b9aed7a0
Brijmohan Dammani <Brijmohan.Dammani@programmers.io>
2024-09-30 18:29:20 +0530

    Added temp for hardcoded keys

---
+++ src/CleanArchitecture.Web/Temp.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace CleanArchitecture.Web
+{
+    public class Temp
+    {
+        public Temp() {
+            // Hardcoded secret information
+            string apiKey = "12345-abcde-67890-fghij"; // Example API key
+            string password = "SuperSecretPassword!"; // Example password
+
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine($"API Key: {apiKey}");
+            Console.WriteLine($"Password: {password}");
+        }
+    }
+}
